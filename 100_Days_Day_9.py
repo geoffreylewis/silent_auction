64c3c838f8109485ab1330304f37ee1233335f4e
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-22 17:08:07 -0500

    Add files via upload
    Beginning project...

---
+++ 100_Days_Day_9.py
@@ -0,0 +1,14 @@
+##################
+# Silent Auction #
+##################
+
+# print("Welcome to the secret auction program.")
+# input("What is your name?")
+# input("What is your bid?")
+# input("Are there any other bidders?  Type \"yes\" or \"no\": ")
+
+# input("What is your name?")
+# input("What is your bid?")
+# input("Are there any other bidders?  Type \"yes\" or \"no\": ")
+
+# print("The winner is Someone with a bid of $Something.")
